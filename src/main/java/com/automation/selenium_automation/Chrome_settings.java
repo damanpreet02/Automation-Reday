e79b9b6c2ba82c20e4e13c5581b3901145d9fe51
hence <hence@192.168.0.188>
2026-03-25 12:58:55 +0530

    fix

--- src/main/java/com/automation/selenium_automation/Chrome_settings.java
+++ src/main/java/com/automation/selenium_automation/Chrome_settings.java
@@ -3,6 +3,7 @@ package com.automation.selenium_automation;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,11 +11,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Chrome_settings {
+	
+	static Scanner sc = new Scanner(System.in);
 
 	protected static WebDriver driver;
 	protected static WebDriverWait wait;
 	static void Chrome_setting() {
-		String Url = "https://qwe.dev.corporate.readytaxxi.com/";
+		
+		System.out.print("Enter Sub Domain");
+		String sub_dom = sc.nextLine();
+		
+		
+		String Url = "https://"+sub_dom+".dev.corporate.readytaxxi.com/";
 		// Configure chrome to allow notifications
 		
 		ChromeOptions options = new ChromeOptions();
